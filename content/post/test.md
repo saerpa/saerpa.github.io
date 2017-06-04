54e1ad1fcf7ac698c7add90d8741d8a2090fda60
saerpa <saerpa@163.com>
2017-06-04 19:55:17 +0800

    modified deploy.sh

---
+++ content/post/test.md
@@ -0,0 +1,9 @@
++++
+categories = ["misc"]
+date = "2017-06-04T19:46:44+08:00"
+tags = ["test"]
+title = "test"
+
++++
+
+# hello, world
--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@ cd ~/blog/saerpa/;
 hugo;
 cd ./public;
 git add --all;
-git commit -m "update";
+git commit -m "pages update";
 #git push -f origin gh-pages;
 git push coding pages:master;
+
+cd ~/blog/saerpa;
+git add --all;
+git commit -m "src update";
+git push coding master:src;
